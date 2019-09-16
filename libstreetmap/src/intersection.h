e2a944b0188160e87f29a52a83ea338aa81fd8cf
willshi88 <32180172+willshi88@users.noreply.github.com>
2019-09-15 21:05:03 -0700

    Update intersection.h

--- libstreetmap/src/intersection.h
+++ libstreetmap/src/intersection.h
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   intersection.h
- * Author: zhan3795
- *
- * Created on January 31, 2018, 7:04 PM
- */
-
 #ifndef INTERSECTION_H
 #define INTERSECTION_H
 #endif /* INTERSECTION_H */
