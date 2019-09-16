0a5943846eb55b7396d0a5927182d420edc5f773
willshi88 <32180172+willshi88@users.noreply.github.com>
2019-09-15 21:04:07 -0700

    Update genetic.h

--- libstreetmap/src/genetic.h
+++ libstreetmap/src/genetic.h
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   genetic.h
- * Author: chenhu49
- *
- * Created on March 31, 2018, 4:23 PM
- */
 
 #ifndef GENETIC_H
 #define GENETIC_H
