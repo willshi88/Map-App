28ea55389bfc2946571772c22f2c65d8fc9187cb
willshi88 <32180172+willshi88@users.noreply.github.com>
2019-09-15 21:04:40 -0700

    Update global.h

--- libstreetmap/src/global.h
+++ libstreetmap/src/global.h
@@ -1,17 +1,4 @@
 #pragma once
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   global.h
- * Author: zhan3114
- *
- * Created on February 5, 2018, 12:49 PM
- */
-
 #ifndef GLOBAL_H
 #define GLOBAL_H
 #include "string"
